6c7063aa427033bba69fe1b48d26498511ff939c
Maksym <maksshykh@gmail.com>
2026-09-18 10:39:18 +0300

    Removed some packages

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import database as data
 
 def good_looking():
-    print(f"Task Number: {data.ID}| Task Name: {data.Name}")
+    print(f"Task Number: {data.ID}| Task Name: {data.Name} | Desc: {data.Desc} | Priority: {data.Priority}")
 
 def task_load(tasks):
     for tasks in data.task_list:
